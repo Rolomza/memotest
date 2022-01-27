3176f543ac9d637d73e3acf09fd8ea8a82d2fe8e
Augusto <augusto.robles.mza@gmail.com>
2022-01-26 23:52:49 -0300

    Creo primeras variables locales y agarro el tablero y los cuadros

---
+++ src/script.js
@@ -0,0 +1,5 @@
+let turnos = 0;
+let $primerCuadro = null;
+const $tablero = document.querySelector('#tablero');
+const $cuadros = document.querySelectorAll('.cuadro');
+const $mensajeFinJuego = document.querySelector('#fin-juego');
