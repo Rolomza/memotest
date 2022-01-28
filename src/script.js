5d8b36d1ca39b3033b854dba3f74b58167ee0f44
Augusto <augusto.robles.mza@gmail.com>
2022-01-28 00:28:40 -0300

    Primeras funciones del juego

--- src/script.js
+++ src/script.js
@@ -1,5 +1,25 @@
 let turnos = 0;
 let $primerCuadro = null;
 const $tablero = document.querySelector('#tablero');
-const $cuadros = document.querySelectorAll('.cuadro');
+const $cuadros = $tablero.querySelectorAll('.cuadro');
 const $mensajeFinJuego = document.querySelector('#fin-juego');
+
+function configurarJuego() {
+  const coloresBase = ['rojo', 'verde', 'azul', 'negro', 'blanco', 'amarillo'];
+  const coloresRepetidos = coloresBase.concat(coloresBase); //Esta linea duplica los colores, 12 colores para los 12 cuadros
+  configurarCuadros($cuadros, coloresRepetidos);
+}
+
+function configurarCuadros($cuadros, colores) {
+  const coloresRandom = colores.sort(function (a, b) {
+    return 0.5 - Math.random();
+  }); //Ordena el array de colores de forma aleatoria
+
+  debugger;
+
+  coloresRandom.forEach(function (color, i) {
+    $cuadros[i].classList.add(color);
+  });
+}
+
+configurarJuego();
