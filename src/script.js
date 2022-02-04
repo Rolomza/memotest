3eaae7be4645db803b46a369a8860f937c269d29
Augusto <augusto.robles.mza@gmail.com>
2022-02-04 00:40:22 -0300

    Comienzo a construir funcion para manejar eventos del tablero

--- src/script.js
+++ src/script.js
@@ -8,14 +8,22 @@ function configurarJuego() {
   const coloresBase = ['rojo', 'verde', 'azul', 'negro', 'blanco', 'amarillo'];
   const coloresRepetidos = coloresBase.concat(coloresBase); //Esta linea duplica los colores, 12 colores para los 12 cuadros
   configurarCuadros($cuadros, coloresRepetidos);
+  manejarEventos($tablero);
+}
+
+function manejarEventos($tablero) {
+  $tablero.onclick = function (e) {
+    debugger;
+    const $elemento = e.target;
+  };
 }
 
 function configurarCuadros($cuadros, colores) {
-  const coloresRandom = colores.sort(function (a, b) {
+  const coloresRandom = colores.sort(function () {
     return 0.5 - Math.random();
   }); //Ordena el array de colores de forma aleatoria
 
-  debugger;
+  //   debugger;
 
   coloresRandom.forEach(function (color, i) {
     $cuadros[i].classList.add(color);
